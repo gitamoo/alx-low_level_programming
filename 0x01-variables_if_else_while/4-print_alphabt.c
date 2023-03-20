0fcfae2bb52d707314d06ef15c2ac95f9298b3c0
gitamoo <ritagitamo@gmail.com>
2023-03-20 14:31:16 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,33 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of two two-digit numbers
- * Return: Always 0 (Success)
+ * main - prints the alphabet in lowercase
+ * followed by a new line, except q and e
+ * Return: Always 0
  */
 int main(void)
 {
-	int i, j;
+	char la;
 
-	for (i = 0; i < 100; i++)
+	for (la = 'a'; la <= 'z'; la++)
 	{
-		for (j = 0; j < 100; j++)
-		{
-			if (i < j)
-			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
-				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
-				if (i != 98 || j != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-
-		}
+		if (la != 'e' && la != 'q')
+			putchar(la);
 	}
+
 	putchar('\n');
+
 	return (0);
-}
+}}
