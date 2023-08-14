29fa24e4c76f58ba6ca0300ad7e44653d1307e25
gitamoo <ritagitamo@gmail.com>
2023-08-14 04:42:02 -0700

    determining size of different data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("char size: %lu bytes\n",sizeof(char));
+	printf("int size: %lu bytes\n",sizeof(int));
+	printf("long int size: %lu bytes\n",sizeof(long int));
+	printf("long long int size: %lu bytes\n",sizeof(long long int));
+	printf("float size: %lu bytes\n",sizeof(float));
+}
+			
