9e42873c82be2fa789684257b0ef5cb63c8bcef3
gitamoo <ritagitamo@gmail.com>
2023-08-20 08:14:46 -0700

    printing digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - printing all single digit numbers
+ * Return: 0 and exit
+ */
+int main(void)
+{
+	int i;
+
+	for (int i = 0; i < 10; i++)
+		putchar(i);
+
+	putchar('\n');
+	return (0);
+}
