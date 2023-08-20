60e7992d1da9bcb4ff2b716668299ae631ca2908
gitamoo <ritagitamo@gmail.com>
2023-08-20 07:51:38 -0700

    printing alphabets except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,9 +7,14 @@
 int main(void)
 {
 	char alphabet;
-	for (alphabet = 'a'; alphabet <= 'z' && alphabet != 'q' && 'e'; alphabet++)
-		putchar(alphabet);
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet != 'e' && alphabet != 'q')
+			putchar(alphabet);
+	}
+
 
 	putchar('\n')
+
 	return (0);
 }
