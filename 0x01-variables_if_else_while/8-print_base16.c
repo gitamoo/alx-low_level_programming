d6047f605828a16195d97fdc54579a55a2e7482f
gitamoo <ritagitamo@gmail.com>
2023-08-20 13:37:22 -0700

    printing no.s of base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,10 +10,14 @@ int main(void)
 	char letter;
 
 	for (i = 0; i <= 9; i++)
+	{		
 		putchar(i);
+	}
 
 	for (letter = 'a'; letter <= 'f'; letter++)
+	{	
 		putchar(letter);
+	}	
 
 	putchar('\n');
 	return (0);
