d6b75bc8c1ae996c25255cdbbb1f0b71d52d8e76
gitamoo <ritagitamo@gmail.com>
2023-08-20 13:28:54 -0700

    print no. of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ * Return: o and exit
+ */
+int main(void)
+{
+	int i;
+	char letters;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i);
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+	return (0);
+}
