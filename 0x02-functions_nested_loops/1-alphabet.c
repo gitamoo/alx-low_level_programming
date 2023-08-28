b892d98fc16351ab2c80c16f7ea87cc0c650fab5
gitamoo <ritagitamo@gmail.com>
2023-08-28 13:52:05 +0300

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+ * main - print the alphabets in lowercase
+ * Return - 0 and exit
+ */
+int main(void)
+{
+	char alphabets;
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+		_putchar(alphabets);
+
+	putchar('\n');
+	return 0;
+}
