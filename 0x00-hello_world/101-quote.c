ab5708f388023b6c0b1c3d8ffeebec15e6d99afd
Rita <142232316+keruboo@users.noreply.github.com>
2023-09-21 18:41:39 +0300

    Update 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    putchar(and that piece of art is useful" - Dora Korpar, 2015-10-19\n);
+            
